dfa0339e741be28eb6bca1041077247059c1a5a2
Nicolas Fedor <fedronn@gmail.com>
2022-03-13 17:25:18 +0000

    feat: add vulkan validation layers

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,2 +1,5 @@
 pub const WINDOW_WIDTH: u32 = 1280;
 pub const WINDOW_HEIGHT: u32 = 720;
+
+pub const ENABLE_VALIDATION_LAYERS: bool = cfg!(debug_assertions);
+pub const VALIDATION_LAYERS: [&'static str; 1] = ["VK_LAYER_KHRONOS_validation"];
