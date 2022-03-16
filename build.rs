022ae122df54c3367bcfe955327e285ce0b9b25c
Nicolas Fedor <fedronn@gmail.com>
2022-03-16 09:21:45 +0000

    build: compile magma shaders
    Any Magma shaders in the 'shaders' directory are compiled as a build
    step so that the application doesn't need to compile shaders at run-time

---
+++ build.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // Build all the shaders in 'shaders' before-hand so the application doesn't need to compile them at runtime
+    let mut shaders_dir = std::env::current_dir().expect("Failed to get current dir");
+    shaders_dir.push("shaders");
+    let entries = std::fs::read_dir(shaders_dir).expect("Failed to get entries in directory");
+
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if path.is_dir() {
+                println!("Compiling {:?} shader crate", path);
+                spirv_builder::SpirvBuilder::new(
+                    format!("shaders/{}", path.file_name().unwrap().to_str().unwrap()),
+                    "spirv-unknown-vulkan1.1",
+                )
+                .print_metadata(spirv_builder::MetadataPrintout::None)
+                .build()
+                .expect(&format!(
+                    "Failed to build shader {:?}",
+                    path.file_name().unwrap()
+                ));
+            }
+        }
+    }
+}
