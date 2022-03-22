a817d3c16a44d97a8e0a77118f521234fa5d9ac7
Nicolas Fedor <fedronn@gmail.com>
2022-03-22 09:02:06 +0000

    refactor: removed everything from app
    Whole engine is going to be restructed since it was getting messy to add
    new features to and it was difficult to read and follow the code.
    
    The app now only consists of creating a winit window and event loop.

--- src/app.rs
+++ src/app.rs
@@ -1,146 +1,60 @@
-use std::rc::Rc;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
-use crate::{
-    camera::Camera,
-    entity::{Entity, Transform},
-    input::KeyboardInput,
-    movement_system,
-    renderer::{device::Device, simple_render_system::SimpleRenderSystem, Renderer},
-    utils,
-};
+use crate::utils;
 
 /// Main application for Magma, and the entry point
 pub struct App {
-    /// Handle to winit window
-    window: Rc<winit::window::Window>,
-    /// Handle to logical device
-    pub device: Rc<Device>,
-    /// Handle to the Vulkan renderer
-    renderer: Renderer,
-    /// List of all entities in the 'world'
-    entities: Vec<Entity>,
-    /// Current size of the window in pixels
-    window_size: winit::dpi::PhysicalSize<u32>,
-    /// Currently pressed keys
-    keyboard_input: KeyboardInput,
+    /// Handle to Window
+    window: Window,
+    /// Handle to winit EventLoop corresponding to the creating window
+    event_loop: EventLoop<()>,
 }
 
 impl App {
     /// Creates a new App
     ///
-    /// Loads the Vulkan library and then creates a new Renderer
-    pub fn new(event_loop: &winit::event_loop::EventLoop<()>) -> App {
-        let window = Rc::new(App::init_window(event_loop));
-        let device = Rc::new(Device::new(&window));
-        let renderer = Renderer::new(window.clone(), device.clone());
-        let window_size = window.inner_size();
+    /// Creates a platform-specific window, loads the Vulkan library and creates a master renderer
+    pub fn new() -> App {
+        let (window, event_loop) = App::init_window();
 
-        App {
-            window,
-            device,
-            renderer,
-            entities: Vec::new(),
-            window_size,
-            keyboard_input: KeyboardInput::new(),
-        }
+        App { window, event_loop }
     }
 
-    /// Initialises a winit window, returning the initialised window
-    pub fn init_window(event_loop: &EventLoop<()>) -> Window {
-        WindowBuilder::new()
+    /// Initialises a winit window
+    ///
+    /// Returns the window, and the event loop used by the window
+    pub fn init_window() -> (Window, EventLoop<()>) {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new()
             .with_title(utils::constants::WINDOW_TITLE)
             .with_inner_size(winit::dpi::LogicalSize::new(
                 utils::constants::WINDOW_WIDTH,
                 utils::constants::WINDOW_HEIGHT,
             ))
-            .build(event_loop)
-            .expect("")
-    }
+            .build(&event_loop)
+            .expect("");
 
-    /// Adds a new entity that will be rendered
-    pub fn add_entity(&mut self, entity: Entity) {
-        self.entities.push(entity);
+        (window, event_loop)
     }
 
     /// Runs the winit event loop, which wraps the App main loop
-    pub fn main_loop(mut self, event_loop: EventLoop<()>) {
-        let simple_render_system = SimpleRenderSystem::new(
-            self.device.clone(),
-            self.renderer.get_swapchain_render_pass(),
-        );
-
-        let mut camera = Camera::from_perspective(
-            cgmath::Deg(50.0).into(),
-            self.renderer.aspect_ratio(),
-            0.1,
-            10.0,
-        );
-        let mut camera_transform = Transform {
-            position: cgmath::Point3::new(0.0, 0.0, 0.0),
-            rotation: cgmath::Point3::new(0.0, 0.0, 0.0),
-            scale: cgmath::Point3::new(1.0, 1.0, 1.0),
-        };
-
-        let delta_time = 1.0 / 60.0;
-        event_loop.run(move |event, _, control_flow| match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Resized(size) => {
-                    self.window_size = size;
-                    self.renderer.recreate_swapchain();
+    pub fn main_loop(self) {
+        self.event_loop
+            .run(move |event, _, control_flow| match event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => {}
+                },
+                Event::MainEventsCleared => {
+                    self.window.request_redraw();
                 }
-                WindowEvent::KeyboardInput { input, .. } => {
-                    self.keyboard_input.register_input(input);
-                }
+                Event::RedrawRequested(_) => {}
+                Event::LoopDestroyed => {}
                 _ => {}
-            },
-            Event::MainEventsCleared => {
-                movement_system::move_in_xz_plane(
-                    &self.keyboard_input,
-                    &mut camera_transform,
-                    delta_time,
-                );
-                //println!("{:#?}", camera_transform);
-                camera.set_view_rotation(
-                    cgmath::Vector3::new(
-                        camera_transform.position.x,
-                        camera_transform.position.y,
-                        camera_transform.position.z,
-                    ),
-                    cgmath::Vector3::new(
-                        camera_transform.rotation.x,
-                        camera_transform.rotation.y,
-                        camera_transform.rotation.z,
-                    ),
-                );
-                self.window.request_redraw();
-            }
-            Event::RedrawRequested(_) => {
-                if let Some(command_buffer) = self.renderer.begin_frame() {
-                    self.renderer.begin_swapchain_render_pass(command_buffer);
-                    simple_render_system.render_entities(
-                        command_buffer,
-                        &mut self.entities,
-                        &camera,
-                    );
-                    self.renderer.end_swapchain_render_pass(command_buffer);
-                    self.renderer.end_frame();
-                }
-            }
-            Event::LoopDestroyed => {
-                unsafe {
-                    self.device
-                        .device
-                        .device_wait_idle()
-                        .expect("Failed to wait until device idle");
-                };
-            }
-            _ => {}
-        });
+            });
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 pub use cgmath;
 
 pub mod app;
-pub mod camera;
-pub mod entity;
-pub mod input;
-pub mod model;
-pub mod movement_system;
 pub mod utils;
 
-mod renderer;
+// mod renderer;
