0c97674504280277e9606d5a7463889bacce9601
Nicolas Fedor <fedronn@gmail.com>
2022-03-22 11:23:26 +0000

    feat(input): winit keyboard input handling
    Added new 'input' crate that processes inputs and provides convenient
    functions for checking the state of an input in an 'InputHandler'.
    
    The 'window' crate handles converting from winit input events to magma
    inputs and registering them in the 'InputHandler'
    
    Only keyboard inputs are gathered for now.

--- crates/magma_window/src/window.rs
+++ crates/magma_window/src/window.rs
@@ -1,8 +1,12 @@
+use magma_input::prelude::{InputHandler, KeyCode};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
+    platform::run_return::EventLoopExtRunReturn,
     window::{Window as WinitWindow, WindowBuilder as WinitWindowBuilder},
 };
+
+use crate::events::convert_winit_keyboard_to_magma;
 
 /// Allows you to configure settings for the [_window] prior to creating
 pub struct WindowBuilder {
@@ -45,6 +49,7 @@ impl WindowBuilder {
         Window {
             _window: window,
             event_loop,
+            inputs: InputHandler::new(),
         }
     }
 }
@@ -52,12 +57,17 @@ impl WindowBuilder {
 pub struct Window {
     _window: WinitWindow,
     event_loop: EventLoop<()>,
+    inputs: InputHandler,
 }
 
 impl Window {
     /// Creates a new [WindowBuilder] with default values
     pub fn builder() -> WindowBuilder {
         WindowBuilder::new()
+    }
+
+    pub fn inputs(self) -> InputHandler {
+        self.inputs
     }
 
     /// Initialises a winit _window and event loop
@@ -76,12 +86,21 @@ impl Window {
 
     /// Runs the winit event loop
     ///
-    /// Blocking operation
-    pub fn run_event_loop(self) {
+    /// Blocking operation but returns once the event loop is quit.
+    pub fn run_event_loop(mut self) {
         self.event_loop
-            .run(move |event, _, control_flow| match event {
+            .run_return(move |event, _, control_flow| match event {
                 Event::WindowEvent { event, .. } => match event {
                     WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput { input, .. } => {
+                        let native_input = convert_winit_keyboard_to_magma(input);
+                        self.inputs.process_keyboard_input(native_input);
+
+                        // TODO: Remove
+                        if self.inputs.is_key_pressed(KeyCode::Escape) {
+                            *control_flow = ControlFlow::Exit
+                        }
+                    }
                     _ => {}
                 },
                 _ => {}
