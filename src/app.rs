77ba855cc68f4f3245176d9b932116e8f3ae2bd6
Nicolas Fedor <fedronn@gmail.com>
2022-03-22 09:13:25 +0000

    feat(app): add app builder
    Moved the creation of an app to a builder for easy configuration

--- src/app.rs
+++ src/app.rs
@@ -4,37 +4,73 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-use crate::utils;
+use crate::utils::constants;
+
+/// Allows you to configure settings for the [App] prior to creating
+pub struct AppBuilder {
+    window_width: u32,
+    window_height: u32,
+    window_title: &'static str,
+}
+
+impl AppBuilder {
+    /// Creates a new [AppBuilder] with default configuration.
+    ///
+    /// See [constants][crate::utils::constants] module for default values
+    pub fn new() -> AppBuilder {
+        AppBuilder {
+            window_width: constants::WINDOW_WIDTH,
+            window_height: constants::WINDOW_HEIGHT,
+            window_title: constants::WINDOW_TITLE,
+        }
+    }
+
+    pub fn window_width(mut self, width: u32) -> AppBuilder {
+        self.window_width = width;
+        self
+    }
+
+    pub fn window_height(mut self, height: u32) -> AppBuilder {
+        self.window_height = height;
+        self
+    }
+
+    pub fn window_title(mut self, title: &'static str) -> AppBuilder {
+        self.window_title = title;
+        self
+    }
+
+    /// Creates a new app from the configuration provided in the builder
+    pub fn build(self) -> App {
+        let (window, event_loop) =
+            App::init_window(self.window_width, self.window_height, self.window_title);
+
+        App { window, event_loop }
+    }
+}
 
 /// Main application for Magma, and the entry point
 pub struct App {
-    /// Handle to Window
+    /// Handle to winit Window
     window: Window,
-    /// Handle to winit EventLoop corresponding to the creating window
+    /// Handle to winit EventLoop corresponding to the created window
     event_loop: EventLoop<()>,
 }
 
 impl App {
-    /// Creates a new App
-    ///
-    /// Creates a platform-specific window, loads the Vulkan library and creates a master renderer
-    pub fn new() -> App {
-        let (window, event_loop) = App::init_window();
-
-        App { window, event_loop }
+    /// Creates a new [AppBuilder] that can be used to configure the values used to create a new [App]
+    pub fn builder() -> AppBuilder {
+        AppBuilder::new()
     }
 
     /// Initialises a winit window
     ///
     /// Returns the window, and the event loop used by the window
-    pub fn init_window() -> (Window, EventLoop<()>) {
+    pub fn init_window(width: u32, height: u32, title: &'static str) -> (Window, EventLoop<()>) {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new()
-            .with_title(utils::constants::WINDOW_TITLE)
-            .with_inner_size(winit::dpi::LogicalSize::new(
-                utils::constants::WINDOW_WIDTH,
-                utils::constants::WINDOW_HEIGHT,
-            ))
+            .with_title(title)
+            .with_inner_size(winit::dpi::LogicalSize::new(width, height))
             .build(&event_loop)
             .expect("");
 
