b365ea57dfaa566cf0f03c25b2bfd1fd2884a8ad
Nicolas Fedor <fedronn@gmail.com>
2022-03-25 15:52:54 +0000

    docs(app): document all current functionality

--- crates/magma_app/src/lib.rs
+++ crates/magma_app/src/lib.rs
@@ -1,3 +1,5 @@
+//! This crate provides high level access to all the sub-crates of as once cohesive application
+
 mod app;
 
 pub mod prelude {
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
+//! Magma is a game engine built in Rust using the Vulkan API that provides you with full control over the rendering process
+
 pub use magma_internal::*;
