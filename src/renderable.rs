7f123590de7c6beed0203affcad3da71f3221e94
Nicolas Fedor <fedronn@gmail.com>
2022-04-02 17:32:51 +0100

    refactor: change structure to use sub-crates
    Another big refactor, yay! Hopefully the last one. Removed all the
    functionality and prepared the crates that will be used to create the
    final 'magma' crate.

--- src/renderable.rs
+++
@@ -1,8 +0,0 @@
-use crate::{components::Transform, mesh::Mesh, pipeline::Pipeline};
-use std::rc::Rc;
-
-pub struct Renderable {
-    pub mesh: Rc<Mesh>,
-    pub pipeline: Rc<Pipeline>,
-    pub transform: Transform,
-}
