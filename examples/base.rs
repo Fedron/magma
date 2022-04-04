382c44de0f06e0b3bbeb87d8455bd53dd7a1f461
Nicolas Fedor <fedronn@gmail.com>
2022-04-04 08:17:33 +0100

    feat: add window and main loop initialisation

--- examples/base.rs
+++ examples/base.rs
@@ -1,8 +1,27 @@
 use magma::prelude::*;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
     simple_logger::SimpleLogger::new()
         .without_timestamps()
         .init()
         .unwrap();
+
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new()
+        .with_title("Example App Base")
+        .build(&event_loop)
+        .expect("Failed to create window");
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    })
 }
