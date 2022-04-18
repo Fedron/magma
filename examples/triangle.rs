2d4af91a9896da486e2151b104330479170a7d8c
Nicolas Fedor <fedronn@gmail.com>
2022-04-18 09:02:38 +0100

    refactor: remove unnecessary examples

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,5 +1,5 @@
-//! Demonstrates how to initialize Vulkan and get a triangle drawing to the screen.
-//! Also supports resizing the window.
+//! Demonstrates how to draw to the screen using a user-defined vertex array
+//! See the triangle example for how setting up Vulkan and Magma works
 
 use std::rc::Rc;
 
@@ -13,63 +13,55 @@ use winit::{
 
 use magma::prelude::*;
 
+#[repr(C)]
+#[derive(Vertex)]
+struct SimpleVertex {
+    #[location = 0]
+    position: [f32; 2],
+    #[location = 1]
+    color: [f32; 3],
+}
+
 fn main() -> Result<()> {
-    // If you want to see log outputs from magma make sure to initialize a logger of your choice
     simple_logger::SimpleLogger::new()
         .without_timestamps()
         .init()
         .unwrap();
 
-    // Magma uses a winit window internally to initialize Vulkan so you will need to create
-    // a winit window and event loop
     let mut event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("Triangle")
+        .with_title("Vertex")
         .build(&event_loop)?;
 
-    // Create an instance that will allow us to interface with Vulkan
     let instance = Instance::new(&[DebugLayer::KhronosValidation])?;
-    // Finds a physical device that is capable of drawing to the screen
-    // We require the swapchain extension and a graphics queue if we want to draw to a surface
     let physical_device = PhysicalDevice::builder()
         .preferred_type(PhysicalDeviceType::CPU)
         .add_queue_family(QueueFamily::new(Queue::Graphics))
         .device_extensions(&[DeviceExtension::Swapchain])
         .build(&instance)?;
-    // Creates a logical device that will allow us to interface with the physical device
     let logical_device = Rc::new(LogicalDevice::new(instance, physical_device)?);
 
-    // The surface will be where we are going to draw to and requires the swapchain device extension
-    // and graphics queue to be enabled on the physical device
     let mut surface = Surface::new(
         logical_device.instance(),
         logical_device.physical_device(),
         &window,
     )?;
-    // Create the swapchain that we will be drawing to
-    // Note: When the window gets resized the swapchain will need to be recreated as its extent
-    // will no longer match that of the window or surface
     let mut swapchain = Swapchain::builder()
         .preferred_color_format(ColorFormat::Srgb)
         .preferred_present_mode(PresentMode::Mailbox)
         .build(logical_device.clone(), &surface)?;
 
-    // We can finally create the graphics pipeline with our shaders
-    let vertex_shader = Shader::new("shaders/triangle.vert")?;
-    let fragment_shader = Shader::new("shaders/triangle.frag")?;
+    let vertex_shader = Shader::new("shaders/vertex.vert")?;
+    let fragment_shader = Shader::new("shaders/vertex.frag")?;
 
-    // A pipeline requires a Vertex generic, but in this case we aren't using a vertex buffer and
-    // so we can set the generic to be `EmptyVertex`, A pipeline also requires a UniformBuffer
-    // generic that will represent a push constant in the shader but since in this case we aren't
-    // using a push constant we can set the generic to be `EmptyPushConstant`
-    let pipeline = Pipeline::<EmptyVertex, EmptyPushConstant>::builder()
+    // Since we are using a Vertex struct to define our vertices, we need the pipeline to use the
+    // same generic type
+    let pipeline = Pipeline::<SimpleVertex, EmptyPushConstant>::builder()
         .attach_shader(vertex_shader)
         .attach_shader(fragment_shader)
         .render_pass(swapchain.render_pass())
         .build(logical_device.clone())?;
 
-    // The command pool will hold one command buffer for each framebuffer we have in the swapchain.
-    // For now we only allocate the command buffers, we will be recording to them each frame
     let mut command_pool = CommandPool::new(
         logical_device.clone(),
         logical_device
@@ -82,18 +74,56 @@ fn main() -> Result<()> {
         CommandBufferLevel::Primary,
     )?;
 
-    // This is our main loop
+    // We create a staging buffer that will allow us to move the vertex data from the host memory
+    // onto the GPU memory which is the fastet memory.
+    //
+    // We could also just store the vertices in a buffer that is both visible to the host and
+    // device but this can start to introduce performance penalties than if you used a device local
+    // buffer.
+    let mut staging_buffer = Buffer::<SimpleVertex, 3>::new(
+        logical_device.clone(),
+        BufferUsageFlags::TRANSFER_SRC,
+        MemoryPropertyFlags::HOST_VISIBLE | MemoryPropertyFlags::HOST_COHERENT,
+    )?;
+    // Before we can write anything to the buffer we need to map it
+    // By specifying the size as `u64::MAX` we tell Vulkan we want to map the whole buffer
+    staging_buffer.map(u64::MAX, 0)?;
+    // After we write, the buffer will automatically be un-mapped
+    staging_buffer.write(&[
+        SimpleVertex {
+            position: [0.0, -0.5],
+            color: [1.0, 0.0, 0.0],
+        },
+        SimpleVertex {
+            position: [-0.5, 0.5],
+            color: [0.0, 1.0, 0.0],
+        },
+        SimpleVertex {
+            position: [0.5, 0.5],
+            color: [0.0, 0.0, 1.0],
+        },
+    ]);
+
+    // We now create a vertex buffer that will only be stored on the GPU and not accesible by us
+    // the host, hence why we need the staging buffer to copy from on the device
+    //
+    // Since we are going to use this buffer as a vertex buffer we need to mark it as such using
+    // the VERTEX_BUFFER buffer usage flag
+    let mut vertex_buffer = Buffer::<SimpleVertex, 3>::new(
+        logical_device.clone(),
+        BufferUsageFlags::TRANSFER_DST | BufferUsageFlags::VERTEX_BUFFER,
+        MemoryPropertyFlags::DEVICE_LOCAL,
+    )?;
+    // Copy from will copy from a buffer using a command buffer on the devcie
+    vertex_buffer.copy_from(&staging_buffer, &command_pool)?;
+
     let mut should_close = false;
     let mut is_minimized = false;
     while !should_close {
-        // Poll for events
         event_loop.run_return(|event, _, control_flow| match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => should_close = true,
                 WindowEvent::Resized(size) => {
-                    // We need to check for the case that the window was minimized for when we want to recreate the
-                    // swapchain as if we try to create framebuffers with a size of (0, 0) then Vulkan will complain
-                    // and the app will crash.
                     if size.width == 0 && size.height == 0 {
                         is_minimized = true
                     } else {
@@ -106,18 +136,11 @@ fn main() -> Result<()> {
             _ => {}
         });
 
-        // If we are minimized, skip the draw loop where would recreate the swapchain since acquire_next_image()
-        // would return SwapchainError::Suboptimal. Skipping the draw loop means we won't try to recreate the swapchain
-        // until the window has a size of greater than (0, 0)
         if is_minimized {
             continue;
         }
 
-        // Draw loop
         let result = swapchain.acquire_next_image();
-        // Usually you will only need to recreate the swapchain if you get back
-        // SwapchainError::Suboptimal although in this case we dediced to recreate it no matter
-        // what error we get
         if result.is_err() {
             swapchain = recreate_swapchain(
                 swapchain,
@@ -129,40 +152,29 @@ fn main() -> Result<()> {
         }
         let image_index = result.unwrap();
 
-        // With the image index we can record the command buffer belonging to that framebuffer.
-        //
-        // You don't need to record your framebuffers every frame but as your rendering gets more
-        // complex and dynamic rerecording every frame may make it easier.
-        //
-        // In this case though, recording the command buffers once when we initialized Vulkan would
-        // have been enough as we don't change any data from frame to frame.
         let command_buffer = command_pool.buffers_mut().get_mut(image_index).unwrap();
-        command_buffer.begin()?; // You need to first begin recording the command buffer
+        command_buffer.begin()?;
         command_buffer.set_clear_color((0.01, 0.01, 0.01));
         command_buffer.begin_render_pass(
             swapchain.render_pass(),
             *swapchain.framebuffers().get(image_index).unwrap(),
             swapchain.extent(),
-        )?; // Make sure to set the render pass first before we set anything else as they get configured for this render pass
+        )?;
 
-        // The swapchain is created with a dynamic viewport and scissor so we need to set them here
-        // to the size of the swapchain
         let extent = swapchain.extent();
         command_buffer.set_viewport(extent.0 as f32, extent.1 as f32)?;
         command_buffer.set_scissor(extent.clone())?;
 
-        // We can finally bind our graphics pipeline and issue the draw command
         command_buffer.bind_pipeline(&pipeline);
+        // Since our pipeline was created with vertex inputs it now expects a vertex buffer to be
+        // bound before we call `draw()`
+        command_buffer.bind_vertex_buffer(&vertex_buffer);
         command_buffer.draw(3, 1, 0, 0);
 
-        // Finally we need to make sure we end the render pass and then the command buffer
         command_buffer.end_render_pass();
         command_buffer.end()?;
 
-        // With our command buffer recorded, we can submit it to the swapchain which will queue the
-        // command buffer and then present the output to the surface
         let result = swapchain.submit_command_buffer(command_buffer, image_index);
-        // Same as before, if we get an error from the swapchain we recreate the swapchain
         if result.is_err() {
             swapchain = recreate_swapchain(
                 swapchain,
@@ -174,9 +186,6 @@ fn main() -> Result<()> {
         }
     }
 
-    // Once we finish our main loop we need to wait for the logical device to idle.
-    // If we don't wait then Vulkan resources will start being destroyed even though the logical
-    // device was still using them
     logical_device.wait_for_idle()?;
 
     Ok(())
@@ -188,21 +197,15 @@ fn recreate_swapchain(
     surface: &mut Surface,
     command_pool: &mut CommandPool,
 ) -> Result<Swapchain> {
-    // Before you try to recreate the swapchain you need to wait for the device to stop using it
     logical_device.wait_for_idle()?;
-    // We know that the window size has changed and so the surface size will have changed as well
-    // But this won't be reflected in the surface's capabilities yet so we need to update those
     surface.update(logical_device.physical_device())?;
 
-    // Create the swapchain just like we do when the app is first run but with the addition of .old_swapchain
     let swapchain = Swapchain::builder()
         .old_swapchain(old_swapchain)
         .preferred_color_format(ColorFormat::Srgb)
         .preferred_present_mode(PresentMode::Mailbox)
         .build(logical_device.clone(), surface)?;
 
-    // If for some reason the new swapchain has a different amount of framebuffers we need to ensure
-    // our command pool has the same amount
     if swapchain.framebuffers().len() != command_pool.buffers().len() {
         command_pool.free_buffers();
         command_pool.allocate_buffers(
